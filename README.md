dee086af61c77f9228606d5e7ad0b036947a6059
Cole Peaden <dcpeaden@protonmail.com>
2021-01-31 22:37:59 -0600

    Basic Website Setup (#1)
    * moved navbar to it's own component.
    
    * added footer component, and added logo.
    
    * added logo rights messaging in footer.
    
    * justified footer to bottom.
    
    * changed nav items to reflect choices of future pages.
    
    * updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RadWOD
+Pet project just for fun to create a user-friendly, performance fitness focused gym website.
--- rad-wod/src/app/app.module.ts
+++ rad-wod/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
